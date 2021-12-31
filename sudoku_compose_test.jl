db7baf3d3876cfb9cfd1ffab8075a96c6a5a556b
Joseph M. Rutherford <joseph.m.rutherford@ieee.org>
2021-12-31 11:25:56 -0600

    Coarsening error tolerance due to RNG sensitivity #3

--- sudoku_compose_test.jl
+++ sudoku_compose_test.jl
@@ -16,7 +16,7 @@ function multiple_random_permutations!(seed,results)
 end
 
 function test_random_permutations()
-    num_tests = 1000000 # number of calls to test
+    num_tests = 4000000 # number of calls to test
     all_results = zeros(Int,(num_tests,4))
     
     # Plan to split the work across all worker
@@ -39,14 +39,15 @@ function test_random_permutations()
                 @view all_results[((i-1)*workload+1):(i*workload),:])
         end
     end
+    MAX_RESIDUAL_ERROR = 2.0/sqrt(num_tests)
     # Verify that the number of each permutation type is consistent
     column_1_counts = [count(i->i==j,@view all_results[:,1]) for j in 1:7]
-    @test abs(column_1_counts[1] - (num_tests/7))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_1_counts[2] - (num_tests/7))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_1_counts[3] - (num_tests/7))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_1_counts[4] - (num_tests/7))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_1_counts[5] - (num_tests/7))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_1_counts[6] - (num_tests/7))/num_tests < 1/sqrt(num_tests)
+    @test abs(column_1_counts[1] - (num_tests/7))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_1_counts[2] - (num_tests/7))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_1_counts[3] - (num_tests/7))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_1_counts[4] - (num_tests/7))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_1_counts[5] - (num_tests/7))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_1_counts[6] - (num_tests/7))/num_tests < MAX_RESIDUAL_ERROR
     
     # Verify that the number of each block selected for permutation 1:4 is consistent
     column_2_counts = zeros(Int,(3,))
@@ -56,9 +57,9 @@ function test_random_permutations()
         end
     end
     # 4/7 of the time we selected a block, and 1/3 of the those were 1, 2, and 3
-    @test abs(column_2_counts[1] - (4*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_counts[2] - (4*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_counts[3] - (4*num_tests/21))/num_tests < 1/sqrt(num_tests)
+    @test abs(column_2_counts[1] - (4*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_counts[2] - (4*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_counts[3] - (4*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
     
     # Verify that the subrow selected for permutations 1, 3 is consistent
     column_3_subrow_counts = zeros(Int,(3,))
@@ -68,9 +69,9 @@ function test_random_permutations()
         end
     end
     # 2/7 of the time we selected a subrow, and 1/3 of the those were 1, 2, and 3
-    @test abs(column_3_subrow_counts[1] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_subrow_counts[2] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_subrow_counts[3] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
+    @test abs(column_3_subrow_counts[1] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_subrow_counts[2] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_subrow_counts[3] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
     
     # Verify that the second subrow selected for permutations 1, 3 is consistent
     column_4_subrow_counts = zeros(Int,(3,))
@@ -81,9 +82,9 @@ function test_random_permutations()
         end
     end
     # 2/7 of the time we selected a subrow, and 1/3 of the those were 1, 2, and 3
-    @test abs(column_4_subrow_counts[1] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_4_subrow_counts[2] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_4_subrow_counts[3] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
+    @test abs(column_4_subrow_counts[1] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_4_subrow_counts[2] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_4_subrow_counts[3] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
     
     # Verify that the second block selected for permutations 2, 4 is consistent
     column_3_block_counts = zeros(Int,(3,))
@@ -94,9 +95,9 @@ function test_random_permutations()
         end
     end
     # 2/7 of the time we selected a second block, and 1/3 of the those were 1, 2, and 3
-    @test abs(column_3_block_counts[1] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_block_counts[2] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_block_counts[3] - (2*num_tests/21))/num_tests < 1/sqrt(num_tests)
+    @test abs(column_3_block_counts[1] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_block_counts[2] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_block_counts[3] - (2*num_tests/21))/num_tests < MAX_RESIDUAL_ERROR
     
     # Verify that the first, second symbol selected for permutation 7 is consistent
     column_2_symbol_counts = zeros(Int,(9,))
@@ -109,26 +110,26 @@ function test_random_permutations()
         end
     end
     # 1/7 of the time we selected a symbol, and 1/9 of the those were 1:9
-    @test abs(column_2_symbol_counts[1] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[2] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[3] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[4] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[5] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[6] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[7] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[8] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_2_symbol_counts[9] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[1] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[2] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[3] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[4] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[5] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[6] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[7] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[8] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
-    @test abs(column_3_symbol_counts[9] - (1*num_tests/63))/num_tests < 1/sqrt(num_tests)
+    @test abs(column_2_symbol_counts[1] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[2] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[3] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[4] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[5] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[6] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[7] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[8] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_2_symbol_counts[9] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[1] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[2] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[3] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[4] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[5] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[6] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[7] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[8] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
+    @test abs(column_3_symbol_counts[9] - (1*num_tests/63))/num_tests < MAX_RESIDUAL_ERROR
 end
 
 @testset "Composition helpers statistics" begin
-    test_random_permutations()
+    @time test_random_permutations()
 end
